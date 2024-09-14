3c5a56e4e8cfe605233b12e53d32f0a5585e1008
Никита Сергеев <notscreamplay73@gmail.com>
2024-09-14 20:14:22 +0400

    lab1

--- Program.fs
+++ Program.fs
@@ -1,2 +1,5 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿// Лабораторная работа 1 – Основы функционального программирования
+// Выполнить задания с использованием функций высших порядков:
+// Для всех чисел от 1 до 100 найти сумму чисел, кратных 3 и 5
+let sum = List.fold (fun acc next -> if (next % 5 = 0) && (next % 3 = 0) then acc + next else acc) 0 [1..100]
+printf "%d" sum
