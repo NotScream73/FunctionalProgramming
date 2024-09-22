e43e142928519aeb09aa86dc427e929b27a8bd7c
Никита Сергеев <notscreamplay73@gmail.com>
2024-09-22 20:30:52 +0400

    lab6

--- Program.fs
+++ Program.fs
@@ -1,2 +1,31 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿// Задать интерпретацию команд и построить первые итерации
+// в задаваемом количестве для следующей L-системы
+// F++F++F++, F -> F-F++F-F
+
+let applyRule symbol =
+    match symbol with
+    | 'F' -> "F-F++F-F"
+    | '+' -> "+"
+    | '-' -> "-"
+    | _ -> string symbol
+
+let rec applyRules (input: string) : string =
+    input |> Seq.map applyRule |> String.concat ""
+
+let rec iterateLSystem (axiom: string) (iterations: int) : string =
+    if iterations = 0 then
+        axiom
+    else
+        let newAxiom = applyRules axiom
+        iterateLSystem newAxiom (iterations - 1)
+
+[<EntryPoint>]
+let main _ =
+    let axiom = "F++F++F++"
+
+    let iterations = 3
+
+    let result = iterateLSystem axiom iterations
+    printfn "Результат после %d итераций: %s" iterations result
+
+    0
