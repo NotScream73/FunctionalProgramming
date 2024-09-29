23a4e9aa9da21dd92f1d18e4cd236363ba90a537
Никита Сергеев <notscreamplay73@gmail.com>
2024-09-29 12:20:48 +0400

    lab6

--- Program.fs
+++ Program.fs
@@ -2,30 +2,62 @@
 // в задаваемом количестве для следующей L-системы
 // F++F++F++, F -> F-F++F-F
 
-let applyRule symbol =
-    match symbol with
-    | 'F' -> "F-F++F-F"
-    | '+' -> "+"
-    | '-' -> "-"
-    | _ -> string symbol
+open System.Drawing
 
-let rec applyRules (input: string) : string =
-    input |> Seq.map applyRule |> String.concat ""
+type Grammar = (char * char list) list
 
-let rec iterateLSystem (axiom: string) (iterations: int) : string =
-    if iterations = 0 then
-        axiom
-    else
-        let newAxiom = applyRules axiom
-        iterateLSystem newAxiom (iterations - 1)
+let FindSubst c (gr: Grammar) = 
+   match List.tryFind (fun (x,S) -> x=c) gr with
+     | Some(x,S) -> S
+     | None -> [c]
+
+let Apply (gr: Grammar) L =
+   List.collect (fun c -> FindSubst c gr) L
+
+let rec NApply n gr L =
+   if n > 0 then
+       let result = Apply gr L
+       NApply (n - 1) gr result
+   else
+       L
+
+let TurtleBitmapVisualizer n delta cmd =
+    let W,H = 1600,1600
+    let b = new Bitmap(W,H)
+    let g = Graphics.FromImage(b)
+    let pen = new Pen(Color.Black)
+    g.Clear(Color.White)
+    let NewCoord (x:float) (y:float) phi =
+       let nx = x+n*cos(phi)
+       let ny = y+n*sin(phi)
+       (nx,ny,phi)
+    let ProcessCommand x y phi = function
+     | 'f' -> NewCoord x y phi
+     | '+' -> (x,y,phi+delta)
+     | '-' -> (x,y,phi-delta)
+     | 'F' -> 
+         let (nx,ny,phi) = NewCoord x y phi
+         g.DrawLine(pen,(float32)x,(float32)y,(float32)nx,(float32)ny)
+         (nx,ny,phi)
+     | _ -> (x,y,phi)     
+  
+    let rec draw x y phi = function
+     | [] -> ()
+     | h::t ->
+         let (nx,ny,nphi) = ProcessCommand x y phi h
+         draw nx ny nphi t
+    draw (float(W)/2.0) (float(H)/2.0) 0. cmd
+    b
 
 [<EntryPoint>]
-let main _ =
-    let axiom = "F++F++F++"
-
+let main argv =
+    let lSystemInitial = "F++F++F++"
+    let lSystemRules = [ ('F', ['F'; '-'; 'F'; '+'; '+'; 'F'; '-'; 'F']) ]
     let iterations = 3
+    let lSystemResult = NApply iterations lSystemRules (lSystemInitial |> Seq.toList)
 
-    let result = iterateLSystem axiom iterations
-    printfn "Результат после %d итераций: %s" iterations result
+    let bitmap = TurtleBitmapVisualizer 40.0 (System.Math.PI / 3.0) lSystemResult
+
+    bitmap.Save("D://system.jpeg")
 
     0
